2b4d69b87bd4e5285a5f70ec050999fc2a075f8a
Brian Bechtel <brianbechtel@outlook.com>
2020-02-18 13:00:24 -0800

    add more tests

---
+++ src/test/java/lock14/datastructures/SequentialDequeTest.java
@@ -0,0 +1,12 @@
+package lock14.datastructures;
+
+import lock14.datastructures.impl.SequentialDeque;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class SequentialDequeTest extends CollectionTest<Integer> {
+
+    public SequentialDequeTest() {
+        super(SequentialDeque::new, SequentialDeque::new, new AtomicInteger()::getAndIncrement);
+    }
+}
